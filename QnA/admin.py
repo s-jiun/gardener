fcf459202021c61b3a86f32571ae47c531c2edb4
DoyunChoi <cdy97777@gmail.com>
2021-08-04 11:23:45 +0900

    Auto stash before merge of "feature/model" and "upstream/develop"

--- QnA/admin.py
+++ QnA/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+
+@admin.register(models.CommunityQuestion)
+class QuestionAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.CommunityAnswer)
+class Answerdmin(admin.ModelAdmin):
+    pass
