6ad9e92ccf518db7803723c2e3563da72b692f1c
DoyunChoi <cdy97777@gmail.com>
2021-08-05 18:44:04 +0900

    [Fix app 'Search']

--- search/models.py
+++ search/models.py
@@ -5,19 +5,19 @@ from django.db import models
 
 class Plant(models.Model):
     name = models.CharField(blank=True, max_length=200)
-    photo = models.ImageField(upload_to='plant', default='base/baseimg.png')
+    photo_url = models.TextField(blank=True)
     # 식물 사진이 없을때 기본 식물 이미지 지정이 필요할 것 같음
-
-    water_period = models.PositiveIntegerField(blank=True, null=True)
+    growth_form = models.CharField(blank=True, max_length=200)
+    care_difficulty = models.CharField(blank=True, max_length=200)
+    management_level = models.CharField(blank=True, max_length=200)
+    water_period_spring = models.CharField(blank=True, max_length=200)
+    water_period_summer = models.CharField(blank=True, max_length=200)
+    water_period_autumn = models.CharField(blank=True, max_length=200)
+    water_period_winter = models.CharField(blank=True, max_length=200)
     # 물주기가 계절별로 다름. 이 부분 집고 넘어가야함.
-
-    growth_velocity = models.PositiveIntegerField(blank=True, null=True)
-    growth_type = models.TextField(blank=True)
-    # 생장형은 건조 식물엔 있는데 실내 식물 자료에는 없습니다.
-
-    sunlight = models.PositiveIntegerField(blank=True, null=True)
-    growth_temp = models.PositiveIntegerField(blank=True, null=True)
-    care_difficulty = models.PositiveIntegerField(blank=True, null=True)
-    humidity = models.PositiveIntegerField(blank=True, null=True)
+    growth_temp = models.CharField(blank=True, max_length=200)
+    sunlight = models.CharField(blank=True, max_length=200)
+    humidity = models.CharField(blank=True, max_length=200)
+    flower = models.CharField(blank=True, max_length=200)
     content = models.TextField(blank=True)
     # season = models.CharField(max_length=20, blank=True) >> 계절정보를 찾지 못함.
