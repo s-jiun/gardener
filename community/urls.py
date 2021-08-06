f64a6b8608772d4ab2a3c1970f75aaa5494d2de6
Jiun <jiun48562@gmail.com>
2021-08-06 15:02:44 +0900

    GAR-12 add community-templates

--- community/urls.py
+++ community/urls.py
@@ -4,11 +4,11 @@ from . import views
 app_name = 'community'
 
 urlpatterns = [
-    path('community/', view=views.post_list, name='post_list'),
-    path('community/<int:pk>/', view=views.post_detail, name='post_detail'),
-    path('community/create/', view=views.post_create, name='post_create'),
-    path('community/<int:pk>/update/',
+    path('', view=views.post_list, name='post_list'),
+    path('post/<int:pk>/', view=views.post_detail, name='post_detail'),
+    path('post/create/', view=views.post_create, name='post_create'),
+    path('post/<int:pk>/update/',
          view=views.post_update, name='post_update'),
-    path('community/<int:pk>/delete/',
+    path('post/<int:pk>/delete/',
          view=views.post_delete, name='post_delete'),
 ]
