3dd621d025008e30f9c2cab94fdfbcd51db5c9a8
SeungJooKim <seoungjoo01@gmail.com>
2021-08-11 10:12:21 +0900

    Add Pagination

--- user/urls.py
+++ user/urls.py
@@ -12,7 +12,6 @@ urlpatterns = [
     path('update/',views.member_modification, name='update'),
     path('profile/<int:pk>',views.profile, name='profile'),
     path('profile/<int:pk>/follow', views.follow_list, name='follow'),
-    # path('profile/<int:pk>/following', views.following_list, name='following'),
     path('profile/update',views.profile_update, name='profile_update'),
     path('myprofile',views.my_profile, name='my_profile'),
     path('',views.start_page, name ="start_page"),
