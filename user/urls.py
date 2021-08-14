56e6b7a1b9ab2bc5d7418f898dbd9e624d5dbb96
SeungJooKim <seoungjoo01@gmail.com>
2021-08-14 11:18:08 +0900

    Add Plant Scrab

--- user/urls.py
+++ user/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path('logout/', views.logout, name='logout'),
     path('signup/', views.signup, name='signup'),
     path('signout/', views.member_del, name='signout'),
-
     path('update/', views.member_modification, name='update'),
     path('profile/<int:pk>', views.profile, name='profile'),
     path('profile/<int:pk>/follow', views.follow_list, name='follow'),
@@ -20,5 +19,6 @@ urlpatterns = [
     path('follow_ajax/', views.follow_ajax, name='follow_ajax'),
     path('find_id/', views.find_id, name='find_id'),
     path('profile/<int:pk>/my_pick', views.liked_posts, name='my_pick'),
-    path('profile/<int:pk>/my_scrab_plant', views.my_scrab_plant, name='my_scrab_plant'),
+    path('profile/<int:pk>/my_scrab_plant', view=views.ScrabListView.as_view(), name='my_scrab_plant'),
+    path('profile/delete_scrab/<int:pk>', view=views.delete_scrab, name ='delete_scrab')
 ]
