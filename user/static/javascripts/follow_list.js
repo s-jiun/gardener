fad8ca166193d216f03ee5010f5d8a31eca6d213
gunom <gu3062@naver.com>
2021-08-23 10:33:56 +0900

    Fix follow list

--- user/static/javascripts/follow_list.js
+++ user/static/javascripts/follow_list.js
@@ -30,7 +30,7 @@ const followingHandleResponse = () => {
       listConatiner.innerHTML += `
       <div class="text-center" style="margin-top: 50px; height: 300px;">
       <h4> 아직 함께하는 가드너가 없네요
-      <img src="{% static 'images/sad.svg' %}" alt="" style="width: 30px; height: 30px;"></h4>
+      <img src="/static/images/sad.svg" alt="" style="width: 30px; height: 30px;"></h4>
       </div>`
     }
   }
@@ -78,7 +78,7 @@ const followHandleResponse = () => {
     </div>
     <div class="col" style="width:80%">
     <h5 id="follow-name">
-    <img src="{% static 'images/ID.png' %}" alt="" style="width:20px; height:20px; margin-bottom: 5px;">
+    <img src="/static/images/ID.png" alt="" style="width:20px; height:20px; margin-bottom: 5px;">
     ID : ${user_userid}</h5>
     <div class="follow-wraper-${user_id}" style="display:inline; ">
     <div class="following-${user_id}" style="display:inline;"> 
@@ -96,5 +96,99 @@ const followHandleResponse = () => {
 requestFollow.onreadystatechange = () => {
   if (requestFollow.readyState === XMLHttpRequest.DONE) {
     followHandleResponse();
+  }
+};
+
+//  not my profile
+const requestOtherFollowing = new XMLHttpRequest();
+
+const onClickOtherFollowing = (user_id) => {
+  const url = "/following_ajax/";
+  requestOtherFollowing.open("POST", url, true);
+  requestOtherFollowing.setRequestHeader(
+    "Content-Type",
+    "application/x-www-form-urlencoded"
+  );
+  requestOtherFollowing.send(JSON.stringify({ user_id: user_id }));
+};
+
+const otherFollowingHandleResponse = () => {
+  if (requestOtherFollowing.status < 400) {
+    const { user_id,  user_name, user_point, user_image_url, user_userid  } = JSON.parse(requestOtherFollowing.response);
+    const element = document.querySelectorAll(`.follow-wraper-${user_id}`);
+    const followingObject = document.querySelector(`.follow-object-${user_id}`)
+    console.log(element);
+    if (element.length > 1){
+      element[0].innerHTML = `
+      <div class="follow-${user_id}" style="display:inline;">
+      <button onclick="onClickOtherFollow(${user_id})" style="background: white; border: 1px solid #ACA790; padding:5px; border-radius:10px; font-size:16px;">팔로우</button>
+      </div>
+      `
+      element[1].innerHTML = `
+      <div class="follow-${user_id}" style="display:inline;">
+      <button onclick="onClickOtherFollow(${user_id})" style="background: white; border: 1px solid #ACA790; padding:5px; border-radius:10px; font-size:16px;">팔로우</button>
+      </div>
+      `
+    }
+    else {
+      element[0].innerHTML = `
+      <div class="follow-${user_id}" style="display:inline;">
+      <button onclick="onClickOtherFollow(${user_id})" style="background: white; border: 1px solid #ACA790; padding:5px; border-radius:10px; font-size:16px;">팔로우</button>
+      </div>
+      `
+    }
+  }
+};
+
+requestOtherFollowing.onreadystatechange = () => {
+  if (requestOtherFollowing.readyState === XMLHttpRequest.DONE) {
+    otherFollowingHandleResponse();
+  }
+};
+
+const requestOtherFollow = new XMLHttpRequest();
+
+const onClickOtherFollow = (user_id) => {
+  const url = "/follow_ajax/";
+  requestOtherFollow.open("POST", url, true);
+  requestOtherFollow.setRequestHeader(
+    "Content-Type",
+    "application/x-www-form-urlencoded"
+  );
+  requestOtherFollow.send(JSON.stringify({ user_id: user_id }));
+};
+
+const otherFollowHandleResponse = () => {
+  if (requestOtherFollow.status < 400) {
+    const { user_id, user_name, user_point, user_image_url, user_userid } = JSON.parse(requestOtherFollow.response); 
+    const element = document.querySelectorAll(`.follow-wraper-${user_id}`);
+    console.log(element);
+    if (element.length>1){
+
+      element[0].innerHTML = `    
+      <div class='following-${user_id}' style="display:inline;">
+      <button onclick="onClickOtherFollowing(${user_id})"  style="background: white; border: 1px solid #ACA790; padding:5px; border-radius:10px; font-size:16px;">팔로잉</button>
+      </div>
+      `;
+      
+      element[1].innerHTML = `    
+      <div class='following-${user_id}' style="display:inline;">
+      <button onclick="onClickOtherFollowing(${user_id})"  style="background: white; border: 1px solid #ACA790; padding:5px; border-radius:10px; font-size:16px;">팔로잉</button>
+      </div>
+      `;
+    }
+    else {
+      element[0].innerHTML = `    
+      <div class='following-${user_id}' style="display:inline;">
+      <button onclick="onClickOtherFollowing(${user_id})"  style="background: white; border: 1px solid #ACA790; padding:5px; border-radius:10px; font-size:16px;">팔로잉</button>
+      </div>
+      `;
+    }
+      
+  }
+};
+requestOtherFollow.onreadystatechange = () => {
+  if (requestOtherFollow.readyState === XMLHttpRequest.DONE) {
+    otherFollowHandleResponse();
   }
 };
