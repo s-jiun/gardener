c1151dd9ba6762458e321271d588fdb29552dc2f
SeungJooKim <seoungjoo01@gmail.com>
2021-09-20 18:25:50 +0900

    Add model (Reply)

--- event/models.py
+++ event/models.py
@@ -15,7 +15,7 @@ class Challenge(models.Model):
     def __str__(self):
         return self.title
 
-class Issue(models.Model):
+class CardNews(models.Model):
     title = models.CharField(max_length=200)
     content = RichTextUploadingField(
         blank=True, null=True, config_name='answer_ckeditor')
@@ -25,3 +25,24 @@ class Issue(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class ChallengeReply(models.Model):  
+    user_id = models.ForeignKey(GeneralUser, on_delete=CASCADE)
+    challenge_id = models.ForeignKey(Challenge, on_delete=CASCADE) 
+    content = RichTextUploadingField(
+        blank=True, null=True, config_name='answer_ckeditor')
+    parent_reply = models.ForeignKey(
+        'self', on_delete=CASCADE, null=True, blank=True, related_name='replies')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class NewsReply(models.Model): 
+    user_id = models.ForeignKey(GeneralUser, on_delete=CASCADE)
+    cardnews_id = models.ForeignKey(CardNews, on_delete=CASCADE) 
+    content = RichTextUploadingField(
+        blank=True, null=True, config_name='answer_ckeditor')
+    parent_reply = models.ForeignKey(
+        'self', on_delete=CASCADE, null=True, blank=True, related_name='replies')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
