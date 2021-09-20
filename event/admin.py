75262c57ab334d419d94665202c090891a48fe51
SeungJooKim <seoungjoo01@gmail.com>
2021-09-20 18:26:30 +0900

    Edit Backend!!! (Add functions)

--- event/admin.py
+++ event/admin.py
@@ -2,11 +2,20 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-@admin.register(models.Issue)
-class IssueAdmin(admin.ModelAdmin):
+@admin.register(models.CardNews)
+class CardNewsAdmin(admin.ModelAdmin):
     pass
 
 
 @admin.register(models.Challenge)
 class ChallengeAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(models.NewsReply)
+class CardNewsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.ChallengeReply)
+class ChallengeReplyAdmin(admin.ModelAdmin):
     pass
