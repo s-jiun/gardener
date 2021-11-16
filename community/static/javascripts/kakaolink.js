4cba02b528044162a3941614ff94301935513213
jiun <jiun48562@gmail.com>
2021-11-17 03:18:59 +0900

    카카오 공유 기능..

--- community/static/javascripts/kakaolink.js
+++ community/static/javascripts/kakaolink.js
@@ -1,24 +1,24 @@
 Kakao.init('673bc569637b7dc9ae3111971d08e04a');
 function sendLinkKakao(){
     Kakao.Link.sendDefault({
-      objectType: 'feed',
-      content: {
-        title: '{{ post.title }}',
-        description: '{{ post.content|truncatewords:15 }}',
-        imageUrl: '"https://ourplant.kr"+"{{post.image.url}}"',
-        link: {
-          mobileWebUrl: '{{ request.build_absolute_uri }}',
-          webUrl: '{{ request.build_absolute_uri }}'
-        }
-      },
-      buttons: [       
-        {
-          title: '링크 열기',
-          link: {
-            mobileWebUrl: '{{ request.build_absolute_uri }}',
-            webUrl: '{{ request.build_absolute_uri }}'
-          }
-        }
-      ]
+        objectType: 'feed',
+        content: {
+            title: '{{ post.title }}',
+            description: '{{ post.content|truncatewords:15 }}',
+            imageUrl: '"https://ourplant.kr"+"{{post.image.url}}"',
+            link: {
+                mobileWebUrl: '{{ request.build_absolute_uri }}',
+                webUrl: '{{ request.build_absolute_uri }}'
+            }
+        },
+        buttons: [       
+            {
+                title: '링크 열기',
+                link: {
+                    mobileWebUrl: '{{ request.build_absolute_uri }}',
+                    webUrl: '{{ request.build_absolute_uri }}'
+                }
+            }
+        ]
     }); 
 }
